3a01b83f8ea41c7c766aab0dc9c7963641f9e61e
Bala.FA <bala.gluster@gmail.com>
2019-10-17 20:58:50 +0530

    add errors

---
+++ datasys/errors/errors.go
@@ -0,0 +1,17 @@
+package errors
+
+import "errors"
+
+var (
+	ErrUploadIDAlreadyExist = errors.New("upload ID already exist")
+	ErrUploadIDNotFound     = errors.New("upload ID not found")
+	ErrPartChecksumNotFound = errors.New("part checksum file not found")
+	ErrPartNotFound         = errors.New("part file not found")
+	ErrDataIDAlreadyExist   = errors.New("data ID already exist")
+	ErrDataIDNotFound       = errors.New("data ID not found")
+)
+
+var (
+	ErrBucketAlreadyExist = errors.New("bucket already exist")
+	ErrBucketNotFound     = errors.New("bucket not found")
+)
